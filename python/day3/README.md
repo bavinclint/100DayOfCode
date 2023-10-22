63d4dfd1f21183020aae72c1cf656b7022cfe985
bavinclint <bavinclint@gmail.com>
2023-10-22 19:46:17 +0300

    initialize pizza order exercise

---
+++ python/day3/README.md
@@ -0,0 +1,10 @@
+## Pizza Order
+
+# Instructions
+
+Small Pizza: $15
+Mediun Pizza: $20
+Large Pizza: $25
+Pepperoni for small pizza: +$2
+Pepperoni for Medium or Large Pizza: +$3
+Extra cheese for any size pizza: +$1
--- python/day3/day-3-4-exercise.py
+++ python/day3/day-3-4-exercise.py
@@ -1,5 +1,52 @@
 # pizza order
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L\n")
+add_pepperoni = input("Do you want pepperoni? Y or N\n")
+extra_cheese = input("Do you want extra cheese? Y or N\n")
+bill = 0
+
+if size == 'S':
+    bill = 15
+    if add_pepperoni == 'Y':
+        bill += 2
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    elif add_pepperoni == 'N':
+        if extra_cheese == 'Y':
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+elif size == 'M':
+    bill = 20
+    if add_pepperoni == 'Y':
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    elif add_pepperoni == 'N':
+        if extra_cheese == 'Y':
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+elif size == 'L':
+    bill = 25
+    if add_pepperoni == 'Y':
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    elif add_pepperoni == 'N':
+        if extra_cheese == 'Y':
+            bill += 1
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
