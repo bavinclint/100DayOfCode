17fb2bbf975adad6d7863a52d270dee86f9890ba
bavinclint <bavinclint@gmail.com>
2023-10-24 16:13:25 +0300

    loop introduction

---
+++ python/day5/password-generator-end.py
@@ -0,0 +1,7 @@
+print("Welcome to the PyPassword Generator!")
+letters = input("How many letters would you like in your password?\n")
+symbols = input("How many symbols would you like?\n")
+numbers = input("How many numbers would you like?\n")
+
+
+print("Here is your password: ")
