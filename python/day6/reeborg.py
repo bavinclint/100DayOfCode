5da4f7300e3ed196cc3a98e808006af14e4e0cf3
bavinclint <bavinclint@gmail.com>
2023-11-30 16:12:33 +0300

    reeborg test

--- python/day6/reeborg.py
+++ python/day6/reeborg.py
@@ -1,1 +1,28 @@
 # https://reeborg.ca/index_en.html
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+        
+def jump():
+    if wall_in_front():
+        turn_left()
+        turn_left()
+        if wall_in_front():
+            turn_left()
+            move()
+        else:
+            move()
+    else:
+        move()
+
+while not at_goal():
+    if is_facing_north():
+        turn_right()
+        if front_is_clear():
+            move()
+        elif wall_in_front():
+            turn_right()
+            jump()
+    else:
+        turn_left()
